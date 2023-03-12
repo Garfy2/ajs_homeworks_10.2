bc044832c9400b80fb59b209c5543bed5e8c57a1
Garfy2 <105547060+Garfy2@users.noreply.github.com>
2023-03-12 22:03:08 +0300

    upload

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import GameSavingLoader from './gamesavingloader';
+
+(async () => {
+  try {
+    const saving = await GameSavingLoader.load();
+    console.log(saving)
+    return saving;
+  } catch (error) {
+    return error;
+  }
+})();
